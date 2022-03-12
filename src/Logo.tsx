a7d33a71ef499ca13eb234506256bd631f6d6df5
Dawid Dudek <dawid.dudek@fingo.pl>
2022-03-12 07:13:48 +0100

    L2

---
+++ src/Logo.tsx
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import React from "react";
+
+function Logo(){
+    return (
+        <div>
+            <img src={logo} className="App-logo" alt="logo" />
+        </div>
+    );
+}
+
+export default Logo
